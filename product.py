eafbe123b1535951df75b4c3d62153c730a3722b
Tsvetkova Nina <tsvnishka@gmail.com>
2023-02-20 15:38:27 +0300

    Create project electronics_store

---
+++ product.py
@@ -0,0 +1,50 @@
+"""
+Создайте класс для представления товара в магазине. Экземпляр класса должен содержать атрибуты:
+
+- название товара
+- цена за единицу товара
+- количество товара в магазине
+
+Создайте два атрибута класса:
+
+- для хранения уровня цен с учетом скидки (например, 0.85, при скидке 15%)
+- для хранения созданных экземпляров класса
+
+Реализуйте методы, позволяющие:
+
+- получить общую стоимость конкретного товара в магазине
+- применить установленную скидку для конкретного товара
+"""
+
+
+class Product:
+    """
+    Создайте два атрибута класса:
+
+    - для хранения уровня цен с учетом скидки (например, 0.85, при скидке 15%)
+    - для хранения созданных экземпляров класса
+    """
+    pay_rate = 1
+    product_all = []
+
+    def __init__(self, name_product, amount_product, count_product):
+        """
+        Экземпляр класса должен содержать атрибуты:
+
+        - название товара
+        - цена за единицу товара
+        - количество товара в магазине
+        """
+
+        self.name_product = name_product
+        self.amount_product = amount_product
+        self.count_product = count_product
+
+        Product.product_all.append(self)
+
+    def calculate_total_price(self):
+        total_price = self.count_product * self.amount_product
+        return total_price
+
+    def apply_discount(self):
+        self.amount_product = self.amount_product * Product.pay_rate
